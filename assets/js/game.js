821203cb92c91673295afcbb51da68a6c4794cba
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-01 16:28:44 -0700

    player can choose to fight or skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,7 +20,8 @@ var enemyAttack = 12;
 const player = {
     name: playerName,
     health: 100,
-    attack:10
+    attack: 10,
+    money: 10
 }
 
 //enemy robot setup
@@ -36,18 +37,45 @@ console.log(`playerName: ${player.name}, playerAttack: ${player.attack}, playerH
 //function to check that healthlevel is above zero. Requires robots to be objects
 
 var checkHealth = (robot) => {
-if(robot.health<=0){
-window.alert(`${robot.name} has died!`);
-} else {
-    window.alert(`${robot.name} still has ${robot.health} health left.`);
+    if (robot.health <= 0) {
+        window.alert(`${robot.name} has died!`);
+    } else {
+        window.alert(`${robot.name} still has ${robot.health} health left.`);
     }
 }
 
+var skipTurn = () => {
+    player.money -= 2;
+    window.alert(`${player.name} has skipped. ${player.name}'s money is now ${player.money}.`);
+}
+var areYouSure = () => {
+    let confirmSkip = window.confirm("Are you sure you want to skip? You'll lose 2 money");
+    // sure = sure.toLowerCase();
+    if (!confirmSkip) {
+        window.alert('You have chosen to fight!');
+        fight();
+    } else {
+        skipTurn();
+    }
+}
 
-//fight function, alerts players that the fight has begun
+//does the player want to fight or skip. If they skip they will lose 2 money
+var toFight = () => {
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this round?');
+    promptFight = promptFight.toLowerCase();
+    if (promptFight === 'fight') {
+        fight();
+    }
+    else if (promptFight === 'skip') {
+        areYouSure();
+    } else {
+        window.alert('Invalid Entry');
+        toFight();
+    }
+}
+
+//fight function
 var fight = () => {
-    //Alert users they are starting the round
-    window.alert('Welcome to Robot Gladiators!');
     //subtract playerAttack value from enemyAttack and store result in enemyAttack
     enemy.health -= player.attack;
     //log message to confirm enemyhealth updated
@@ -58,6 +86,15 @@ var fight = () => {
     //log results to ensure playerHealth updated
     console.log(`${enemy.name} attacked ${player.name}. ${player.name} now has ${player.health} health remaining.`);
     checkHealth(player);
+}
+
+
+//play the game
+var playGame = () => {
+    //Alert users they are starting the round
+    window.alert('Welcome to Robot Gladiators!');
+    //does player want to fight or skip
+    toFight();
 };
 
-fight();
+playGame();
